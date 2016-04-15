d08720faf2dd82c3bbc8ae79a716555bd2bc4863
Markus Hanses <me@markushanses.de>
2016-04-15 08:48:08 +0200

    Initial commit with bare spring boot security project.

---
+++ spring-security-oauth/src/main/java/de/demo/security/SpringSecurityOauthApplication.java
@@ -0,0 +1,12 @@
+package de.demo.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauthApplication.class, args);
+	}
+}
---
+++ spring-security-oauth/src/test/java/de/demo/security/SpringSecurityOauthApplicationTests.java
@@ -0,0 +1,16 @@
+package de.demo.security;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringSecurityOauthApplication.class)
+public class SpringSecurityOauthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
