bced0f92389a4058bac645001352baffd4cb874a
Markus Hanses <me@markushanses.de>
2016-04-17 21:17:29 +0200

    AngularJs added to dependencies and used an angular controller to set content.

--- spring-security-oauth/src/main/java/de/demo/security/SpringSecurityOauthApplication.java
+++ spring-security-oauth/src/main/java/de/demo/security/SpringSecurityOauthApplication.java
@@ -2,11 +2,26 @@ package de.demo.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @SpringBootApplication
+@RestController
 public class SpringSecurityOauthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityOauthApplication.class, args);
-	}
+    @RequestMapping("/resource")
+    public Map<String, Object> home() {
+        Map<String, Object> model = new HashMap<>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOauthApplication.class, args);
+    }
 }
